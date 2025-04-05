b54cd17bbe014cffc4719bf7ec4ee0a00a3b6e90
Eric F <erifl321@student.liu.se>
2025-04-05 13:02:00 +0200

    Initial commit. 	-	First implementation that works during simulation.

--- src/project.vhdl
+++ src/project.vhdl
@@ -16,11 +16,85 @@ entity tt_um_example is
 end tt_um_example;
 
 architecture Behavioral of tt_um_example is
+
+    signal mat3x3_0     :   unsigned(71 downto 0) := (others => '0');
+    signal mat3x3_1     :   unsigned(71 downto 0) := (others => '0');
+    signal driver_count :   unsigned( 4 downto 0) := (others => '0');
+    signal shift_count  :   unsigned( 1 downto 0) := (others => '0');
+
 begin
 
     --uo_out <= std_logic_vector(unsigned(ui_in) + unsigned(uio_in));
-    uo_out <= not (ui_in and uio_in);
     uio_out <= "00000000";
     uio_oe <= "00000000";
+
+    process(clk, rst_n)
+    begin
+
+        if rst_n = '0' then
+            mat3x3_0        <=  (others => '0');
+            mat3x3_1        <=  (others => '0');
+            driver_count    <=  (others => '0');
+        elsif rising_edge(clk) then
+            
+            driver_count <= driver_count + 1;
+            if driver_count < 10 then
+               
+                --  inputs
+                mat3x3_0(7  downto 0)  <= ui_in(7 downto 0);
+                mat3x3_1(7  downto 0)  <= uio_in(7 downto 0);
+                --  rotations
+                mat3x3_0(71 downto 8) <= mat3x3_0(63 downto  0);
+                mat3x3_1(71 downto 8) <= mat3x3_1(63 downto  0);                
+
+            else    --  0000 1001,  0000 1010,  0000 1011, 0000 1100
+                --  output                
+                --mat3x3_0(7  downto 0) <= mat3x3_0(71 downto 64);
+                --mat3x3_1(7  downto 0) <= mat3x3_1(71 downto 64);  
+                
+                uo_out <= 
+                    mat3x3_0( 7 downto  0) * mat3x3_1( 7 downto   0) +
+                    mat3x3_0(15 downto  8) * mat3x3_1(31 downto  24) +
+                    mat3x3_0(23 downto 16) * mat3x3_1(55 downto  48)
+                ;
+                
+                --  colum shift left every clock cycle
+                --  row 0
+                mat3x3_1(15 downto  0) <= mat3x3_1(23 downto 8);
+                mat3x3_1(23 downto 16) <= mat3x3_1( 7 downto 0);
+                --  row 1
+                mat3x3_1(39 downto 24) <= mat3x3_1(47 downto 32);
+                mat3x3_1(47 downto 40) <= mat3x3_1(31 downto 24);
+                --  row 2
+                mat3x3_1(63 downto 48) <= mat3x3_1(71 downto 56);
+                mat3x3_1(71 downto 64) <= mat3x3_1(55 downto 48);
+
+                shift_count <= shift_count + 1;
+                if shift_count(1 downto 0) = "10" then
+                    --  row shift up every fourth clock cycle
+                    --  colum 0
+                    mat3x3_0( 7 downto  0) <= mat3x3_0(31 downto 24);
+                    mat3x3_0(31 downto 24) <= mat3x3_0(55 downto 48);  
+                    mat3x3_0(55 downto 48) <= mat3x3_0( 7 downto 0);                   
+                    --  colum 1
+                    mat3x3_0(15 downto  8) <= mat3x3_0(39 downto 32);
+                    mat3x3_0(39 downto 32) <= mat3x3_0(63 downto 56);  
+                    mat3x3_0(63 downto 56) <= mat3x3_0(15 downto  8);
+                    --  colum 1
+                    mat3x3_0(23 downto 16) <= mat3x3_0(47 downto 40);
+                    mat3x3_0(47 downto 40) <= mat3x3_0(71 downto 64);  
+                    mat3x3_0(71 downto 64) <= mat3x3_0(23 downto 16);
+                    shift_count <= "00";
+                end if;
+
+            end if;
+
+
+        end if;
+
+    end process;
+
+
+
 
 end Behavioral;
--- test/test.py
+++ test/test.py
@@ -25,17 +25,75 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
+
+
+    #   tests   
+
+    #   send input
+    #   row 0
+    dut.ui_in.value = 2     #   mat3x3_0
+    dut.uio_in.value = 3    #   mat3x3_1
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 7
+    dut.uio_in.value = 0
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 3
+    dut.uio_in.value = 1
+    await ClockCycles(dut.clk, 1)
+    #   row 1
     dut.ui_in.value = 1
     dut.uio_in.value = 2
-
-    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 5
+    dut.uio_in.value = 1
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 8
+    dut.uio_in.value = 0
+    await ClockCycles(dut.clk, 1)
+    #   row 2
+    dut.ui_in.value = 0
+    dut.uio_in.value = 1
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 4 #
+    dut.uio_in.value = 2
+    await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 1
+    dut.uio_in.value = 4
     await ClockCycles(dut.clk, 1)
 
+    #   read outputs
+    #   row 0
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 23
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 13
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 14
+    #   row 1
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 21
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 21
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 33
+    #   row 2
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 9
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 6
+    await ClockCycles(dut.clk, 1)
+    #assert dut.uo_out.value == 4
+    await ClockCycles(dut.clk, 10)
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     #assert dut.uo_out.value == 50
-    assert dut.uo_out.value == 255
+    #assert dut.uo_out.value == 255
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    #   
+    #       2,  7,  3               3,  0,  1               23, 13, 14
+    #   (   1,  5,  8   )   *   (   2,  1,  0   )   =   (   21, 21, 33  )
+    #       0,  4,  1               1,  2,  4                9,  6,  4
+    #
